92196a561f18cf4b573c4e88ee1bfb2cf2306ece
syauqitaqiyuddin <ahmadsyauqi150@gmail.com>
2023-09-21 15:27:06 +0700

    Push register page & Login page aplikasi eating go

---
+++ app/src/main/java/com/example/eatinggo/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.eatinggo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_page)
+    }
+}
