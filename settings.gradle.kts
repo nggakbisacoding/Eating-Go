481f07853f49cf172c7bc0a00c388cf16e43a603
Fajar W. N <fajarwahyunugroho0802@mail.ugm.ac.id>
2023-11-01 21:59:45 +0700

    Added toolchain management to triggert CodeQL

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,16 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.6.0"
 }
 
+toolchainManagement {
+    jvm {
+        javaRepositories {
+            repository("foojay") {
+                resolverClass.set(org.gradle.toolchains.foojay.FoojayToolchainResolver::class.java)
+            }
+        }
+    }
+}
+
 pluginManagement {
     repositories {
         google()
