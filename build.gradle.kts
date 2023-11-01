f67b7fdd353954b16326593c95d6a5e44fc2d978
FandeadAlter <93658352+FandeadAlter@users.noreply.github.com>
2023-11-01 22:21:23 +0700

    Added plugin foojay

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
