a924498fc8d06c3d3a0f8b0754d491c5b0c09874
sukiceng <ahmadsyauqi150@gmail.com>
2023-11-09 14:31:43 +0700

    Employee Model App Layout

---
+++ app/src/main/java/com/example/eatinggo/HomepageEmployeeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.eatinggo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomepageEmployeeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.homepage_employee)
+    }
+}
---
+++ app/src/main/java/com/example/eatinggo/ReservationDetailPageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.eatinggo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReservationDetailPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.reservation_detail_page)
+    }
+}
