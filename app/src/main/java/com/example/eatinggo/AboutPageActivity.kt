d3d542ccb1118c12ef9829dac8784989f9381940
sukiceng <ahmadsyauqi150@gmail.com>
2023-11-23 14:42:42 +0700

    added: account detail

---
+++ app/src/main/java/com/example/eatinggo/AboutPageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.eatinggo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.about_page)
+    }
+}
---
+++ app/src/main/java/com/example/eatinggo/AccountDetailPageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.eatinggo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AccountDetailPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.account_detail_page)
+    }
+}
