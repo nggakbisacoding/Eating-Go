20aecd50db3551b2e1d3ded04f1f5a76228e1a20
nggakbisacoding <fajarwahyunugroho0802@mail.ugm.ac.id>
2023-11-26 22:06:54 +0700

    Initial Database

---
+++ app/src/main/java/com/example/eatinggo/model/Cafe.kt
@@ -0,0 +1,19 @@
+package com.example.eatinggo.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.sql.Time
+
+@Entity(tableName = "cafe")
+data class Cafe(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo(name = "name") val name: String?,
+    @ColumnInfo(name = "owner") val ownerName: String?,
+    @ColumnInfo(name = "location") val cafeLocation: String?,
+    @ColumnInfo(name = "total_seat") val totalSeat: Int,
+    @ColumnInfo(name = "open_hour") val openHour: Time,
+    @ColumnInfo(name = "close_hour") val  closeHour: Time,
+    @ColumnInfo(name = "reservation_available") val reservationAvailable: Boolean? = false,
+    @ColumnInfo(name = "is_open") val cafeOpen: Boolean
+)
