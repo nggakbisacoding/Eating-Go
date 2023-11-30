633d58955b9c34daaafbf6e5f1e50b29103b183e
sukiceng <ahmadsyauqi150@gmail.com>
2023-11-30 14:06:18 +0700

    added: recyler view for DB integration

---
+++ app/src/main/java/com/example/eatinggo/SplashPageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.eatinggo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_page)
+    }
+}
